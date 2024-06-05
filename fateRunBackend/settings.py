d5fb342ba750ba8a474cacd457f47503f1dd332f
Brandon Brodrick <b.cmoderndesigns@gmail.com>
2024-06-04 20:53:18 -0700

    Redis queuing of ai generation & polling endpoint

--- fateRunBackend/settings.py
+++ fateRunBackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,6 +82,10 @@ REST_FRAMEWORK = {
     ),
 }
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),
+}
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
