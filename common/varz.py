800de8cf315ac5d4efe2dd89d3ab4d2d5f14711b
Brandon Brodrick <b.cmoderndesigns@gmail.com>
2024-06-09 10:33:01 -0700

    more config cleaning up

--- common/varz.py
+++ common/varz.py
@@ -1,3 +1,5 @@
 STATIC_MUSIC_PATH = "/home/brandon/Projects/Python-projects/running-backend/static/"
 STATIC_HOSTNAME = "http://192.168.0.17:8000/assets/"
 GENERATE_ENDPOINT = "http://127.0.0.1:11434/api/generate"
+REDIS_HOST = "127.0.0.1"
+REDIS_PORT = 6379
