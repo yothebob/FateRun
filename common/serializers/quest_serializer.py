d6b7a7c9172eb9385370bad5e637e8df9b371822
Brandon Brodrick <b.cmoderndesigns@gmail.com>
2024-07-17 20:54:37 -0700

    fix quest serializer

--- common/serializers/quest_serializer.py
+++ common/serializers/quest_serializer.py
@@ -1,6 +1,6 @@
 import json
 from django.urls import path, include
-from common.models import Quest
+from common.models import Quest, DialogList
 from rest_framework import serializers
 
 # Serializers define the API representation.
@@ -9,7 +9,10 @@ class QuestSerializer(serializers.ModelSerializer):
     dialogs = serializers.SerializerMethodField()
 
     def get_dialogs(self, obj):
-        return json.loads(obj.dialog_list.dlist)
+        dialog_list = DialogList.object.filter(quest__id=obj.id).first()
+        if dialog_list:
+            return json.loads(dialog_list.dlist)
+        return []
     
     class Meta:
         model = Quest
